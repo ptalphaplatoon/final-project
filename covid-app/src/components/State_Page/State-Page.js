2a53582b7a3f0803c3a344a4dfd357779be8803f
Jarred Simon <jarredcsimon@gmail.com>
2020-11-14 13:45:14 -0600

    tweaked state template slightly

--- covid-app/src/components/State_Page/State-Page.js
+++ covid-app/src/components/State_Page/State-Page.js
@@ -6,7 +6,7 @@ function State_Page(props){
     <div id="StatePage-container">
       <div className="a-api_feed_title">PUBLIC COMMENT</div>
       <div className="a-api_feed_container"></div>
-      <div className="a-state_name_container">Oklahoma</div>
+      <div className="a-state_name_container">OKLAHOMA</div>
       <div className="a-state_info_container"></div>
     </div>
 
