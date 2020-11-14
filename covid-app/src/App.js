d61b48218f9880e477e748526165e4a77457cc37
Jarred Simon <jarredcsimon@gmail.com>
2020-11-14 16:30:24 -0600

    updated router on index and app js

--- covid-app/src/App.js
+++ covid-app/src/App.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import { Route, Switch, Router } from 'react-router-dom'
-import statePage from './components/State_Page/State-Page.js'
+import { Route, Switch } from 'react-router-dom'
+
+import StatePage from './components/State_Page/State-Page.js'
+import HomePage from './pages/HomePage/HomePage.js'
 
 
 
-function App() {
+function App(props) {
   
+  const renderStatePage =(props)=>{
+    return(
+      <StatePage/>
+    )
+  }
 
-  
+  const renderHomePage =(props)=>{
+    return(
+      <HomePage/>
+    )
+  }
+  console.log(props)
   return (
     <div >
-      <Router>
+      {/* <Router> */}
         <Switch>
-          <Route exact path="/state-page" component={statePage} />
+          <Route exact path="/" render={renderHomePage} />
+          <Route exact path="/state-page" render={renderStatePage} />
         </Switch>
-      </Router>
+      {/* </Router> */}
     </div>
     
   );
