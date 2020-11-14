fa54e26fbd961a8a743ed7532db1f8e37c2b5553
Jarred Simon <jarredcsimon@gmail.com>
2020-11-14 13:07:10 -0600

    added state page template with css

---
+++ covid-app/src/components/State_Page/State-Page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './State-PageCss.css'
+
+function State_Page(props){
+  return(
+    <div id="statePage-container">
+      <div className="a-api_feed_title"></div>
+      <div className="a-api_feed_container"></div>
+      <div className="a-state_name_container"></div>
+      <div className="a-state_info_container"></div>
+    </div>
+
+  )
+}
+
+export default State_Page
