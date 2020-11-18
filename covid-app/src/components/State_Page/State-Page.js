01926371d49263b568bade2e33983d42053d1726
Jarred Simon <jarredcsimon@gmail.com>
2020-11-17 19:13:42 -0600

    added Comments component as well as an add comment button on the state-page.

--- covid-app/src/components/State_Page/State-Page.js
+++ covid-app/src/components/State_Page/State-Page.js
@@ -7,7 +7,8 @@ function State_Page(props){
     <div id="StatePage-container">
       <div className="a-api_feed_title">PUBLIC COMMENT</div>
       <div className="a-api_feed_container"></div>
-      <div className="a-state_name_container"><h2>{STATENAME}</h2></div>
+      <div className="a-api_add_feed_button">Add Comment</div>
+      <div className="a-state_name_container">{STATENAME}</div>
       <div className="a-state_info_container"></div>
     </div>
 
