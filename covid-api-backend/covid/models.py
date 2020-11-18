6b548a99e74f1c85f65a4e54f83284632f0ea8ea
blbranch <blbranch@gmail.com>
2020-11-17 21:27:35 -0500

    added Posts model

--- covid-api-backend/covid/models.py
+++ covid-api-backend/covid/models.py
@@ -1,2 +1,13 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
+class Posts(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user + ' ' + self.title  
+        
