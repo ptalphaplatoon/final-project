24138f53b19c061247f5eb350f28233d18f8552a
abegilerova <abegilerova@gmail.com>
2020-11-21 10:42:49 -0600

    missed import

--- covid-api-backend/covid/views.py
+++ covid-api-backend/covid/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer, UserSerializerWithToken
+from .models import Posts
 from .serializers import PostsSerializer
 
 class PostsViewSet(viewsets.ModelViewSet):
