320e82b18cebc34add1ab8841ea9ed683cebdf80
verovillar <veronica25@gmail.com>
2020-11-21 07:34:44 -0800

    fix import for url

--- covid-api-backend/covid_api/urls.py
+++ covid-api-backend/covid_api/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('covid/', include('covid.urls')),
     path('token-auth/', obtain_jwt_token),
-    url(r'^health/', include('health_check.urls')),
+    re_path(r'^health/', include('health_check.urls')),
 ]
