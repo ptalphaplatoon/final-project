7697263d0477c517f6e22665466faf84dbc3fc57
abegilerova <abegilerova@gmail.com>
2020-11-21 10:28:01 -0600

    seralizer for posts to create api requests

--- covid-api-backend/covid/serializers.py
+++ covid-api-backend/covid/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
 from rest_framework_jwt.settings import api_settings
 from django.contrib.auth.models import User
+from .models import Posts
+
+class PostsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Posts
+        fields = ['pk','title', 'description', 'created_at', 'author']
 
 
 class UserSerializer(serializers.ModelSerializer):
--- covid-api-backend/covid/views.py
+++ covid-api-backend/covid/views.py
@@ -7,6 +7,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import UserSerializer, UserSerializerWithToken
+from .serializers import PostsSerializer
+
+class PostsViewSet(viewsets.ModelViewSet):
+    queryset = Posts.objects.all()
+    serializer_class = PostsSerializer
 
 
 @api_view(['GET'])
