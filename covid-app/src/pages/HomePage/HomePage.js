9e0b92d306dda8533ffddffe64807c42b8befe1c
blbranch <blbranch@gmail.com>
2020-12-10 20:51:53 -0500

    made minor changes to homepage

--- covid-app/src/pages/HomePage/HomePage.js
+++ covid-app/src/pages/HomePage/HomePage.js
@@ -184,7 +184,7 @@ function HomePage(props) {
                   screenName: "CDCgov",
                 }}
                 options={{
-                  height: "800",
+                  height: "1000",
                 }}
               />
             </CardBody>
