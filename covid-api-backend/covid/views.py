078cfb32906aca43f9d7a786e8e6073954873956
verovillar <veronica25@gmail.com>
2020-12-12 11:27:54 -0800

    Comment out avatar backend stuff for now

--- covid-api-backend/covid/views.py
+++ covid-api-backend/covid/views.py
@@ -10,7 +10,7 @@ from .serializers import UserSerializer, UserSerializerWithToken
 from .models import Posts
 
 from .serializers import PostsSerializer, UserAvatarSerializer
-from rest_framework.parsers import MultiPartParser, FormParse
+# from rest_framework.parsers import MultiPartParser, FormParse
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class PostsViewSet(viewsets.ModelViewSet):
@@ -51,16 +51,16 @@ class UserList(APIView):
 #             profile = Profile.objects.get(user=request.user)
 #             serializer = ProfileSerializer(profile, data=request.data, partial=True)
 
-class UserAvatarUpload(APIView):
+# class UserAvatarUpload(APIView):
     
-    parser_classes = [MultiPartParser, FormParser]
-    permission_classes = [IsAuthenticated]
+#     parser_classes = [MultiPartParser, FormParser]
+#     permission_classes = [IsAuthenticated]
 
-    def post(self, request, format=None):
-        serializer = UserAvatarSerializer(data=request.data, instance=request.user)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#     def post(self, request, format=None):
+#         serializer = UserAvatarSerializer(data=request.data, instance=request.user)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         else:
+#             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
--- covid-api-backend/covid_api/urls.py
+++ covid-api-backend/covid_api/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from rest_framework_jwt.views import obtain_jwt_token
 from django.conf import settings
 from django.views.generic import TemplateView
-from covid.views import UserAvatarUpload
+# from covid.views import UserAvatarUpload
 from rest_framework.authtoken.views import obtain_auth_token
 
 
@@ -14,7 +14,7 @@ urlpatterns = [
     path('covid/', include('covid.urls')),
     path('token-auth/', obtain_jwt_token),
     path("api/auth-token/", obtain_auth_token, name="rest_auth_token"),
-    path("api/user-avatar/", UserAvatarUpload.as_view(), name="rest_user_avatar_upload"),
+    # path("api/user-avatar/", UserAvatarUpload.as_view(), name="rest_user_avatar_upload"),
     re_path(r'^health/', include('health_check.urls')),
 ]
 
