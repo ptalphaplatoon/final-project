84d2776e8746aa0edd1be263700575a017d923c1
Jarred Simon <jarredcsimon@gmail.com>
2020-12-14 07:20:44 -0600

    set links back to heroku in settings.py

--- covid-api-backend/covid_api/settings.py
+++ covid-api-backend/covid_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-# import django_heroku
+import django_heroku
 from pathlib import Path
 import datetime
 import os
@@ -161,4 +161,4 @@ JWT_AUTH = {
     'JWT_EXPIRATION_DELTA': datetime.timedelta(hours=3),
 }
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
