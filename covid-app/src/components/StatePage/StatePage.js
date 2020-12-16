e0ff8d6010dddadb1d745e8af5a03eec1166d6d2
verovillar <veronica25@gmail.com>
2020-12-16 08:18:55 -0800

    CSS changes, move historic values above comments

--- covid-app/src/components/StatePage/StatePage.js
+++ covid-app/src/components/StatePage/StatePage.js
@@ -194,7 +194,7 @@ function StatePage(props){
 
   return (
     <div>
-      <h1 className="float-container-statename">{stateName}</h1>
+      <h1 className="float-container-statename" style={{paddingLeft: "30px"}}>{stateName}</h1>
       {/* Positive cases, tests, and total tests info boxes */}
       <div>
         <Fragment>
@@ -202,21 +202,19 @@ function StatePage(props){
         </Fragment>
       </div>
       <div className="float-container-twitterandcomments">
-        {/* Card with comments from locals */}
-        <div className="a-api_feed_container float-child-2-comments">
+        {/* Historic values chart */}
+        <div className='float-container-graph'>
           <Fragment>
-            <Card className="card-box mb-5">
-              <div className="card-header pr-2">
-                <div className="text-uppercase card-header--title">
-                  Insight from Locals
+            <Card className='card-box mb-5 p-3 text-center historic-values-card'>
+              <div className="my-3">
+                <h6 className="font-weight-bold font-size-lg mb-1 text-black">
+                  Historic Values Last 60 Days
+                </h6>
+                <div className="state-page-line-chart">
+                  <StatePageChart
+                    historicSingleStateValues={historicSingleStateValues}
+                  />
                 </div>
-                <div className="card-header--actions"></div>
-              </div>
-              <CardBody>
-                <div className="table-responsive-md">{displayComments()}</div>
-              </CardBody>
-              <div className="card-footer d-flex justify-content-between">
-                <div className="a-api_add_feed_button">{showAddComment()}</div>
               </div>
             </Card>
           </Fragment>
@@ -227,7 +225,7 @@ function StatePage(props){
           <Fragment>
             <Card className="card-box mb-5">
               <div className="card-header d-block">
-                <span className="text-uppercase py-3 py-xl-4 text-black d-block text-center font-weight-bold font-size-lg">
+                <span className="py-3 py-xl-4 text-black d-block text-center font-weight-bold font-size-lg">
                   {stateName} Health Department
                 </span>
                 <div className="text-center">{statesCovid19HealthWebsite}</div>
@@ -246,24 +244,26 @@ function StatePage(props){
             </Card>
           </Fragment>
         </div>
-        <div className='float-container-graph'>
+                {/* Card with comments from locals */}
+                <div className="a-api_feed_container float-child-2-comments">
           <Fragment>
-            <Card className='card-box mb-5 p-3 text-center historic-values-card'>
-              <div className="my-3">
-                <h6 className="font-weight-bold font-size-lg mb-1 text-black">
-                  Historic Values Last 60 Days
-                </h6>
-                <div className="state-page-line-chart">
-                  <StatePageChart
-                    historicSingleStateValues={historicSingleStateValues}
-                  />
+            <Card className="card-box mb-5">
+              <div className="card-header pr-2">
+                <div className="py-3 py-xl-4 text-black d-block text-center font-weight-bold font-size-lg">
+                  Insight from Locals
                 </div>
+                <div className="card-header--actions"></div>
+              </div>
+              <CardBody>
+                <div className="table-responsive-md">{displayComments()}</div>
+              </CardBody>
+              <div className="card-footer d-flex justify-content-between">
+                <div className="a-api_add_feed_button">{showAddComment()}</div>
               </div>
             </Card>
           </Fragment>
         </div>
       </div>
-      {/* Historic values chart */}
     </div>
   );
 }
